c3b0cf0731b8d4ac7539674e4c76fa40dbfb9fb9
Ahmed Abdelkarim <30955321+Ahmed-Abdelkarim@users.noreply.github.com>
2019-04-13 13:33:55 +0200

    Controller Class
    This controller class contains the functions required for running the App. The current Functions:
    1- Insert Farm
    2- Update Fish Type
    3- View All Farms
    4- View Farm for Doctors only.

---
+++ Controller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Company
+{
+    public class Controller
+    {
+        DBManager dbMan;
+        public Controller()
+        {
+            dbMan = new DBManager();
+        }
+
+        public int InsertFarm(string ID, string Size, string City, string FarmState, string Automated, string PH, string DO)
+        {
+            string query = "INSERT INTO FishFarm " +
+                            "Values ('" + ID + "'," + Size + ",'" + City + "','" + FarmState + "','" + Automated + "'," + PH + "," + DO + ");";
+
+            return dbMan.UpdateData(query);
+        }
+
+        public int UpdateFishType(string FType, string ID)
+        {
+            string query = "UPDATE FishType SET FTYPE='" + FType + "' WHERE ID='" + ID + "';";
+            return dbMan.UpdateData(query);
+        }
+
+        public DataTable ViewAllFarms()
+        {
+
+            string query = "SELECT * FROM FishFarm;";
+            return dbMan.ExecuteTableQuery(query);
+        }
+        public DataTable DoctorView(string ID)
+        {
+
+            string query = "CREATE VIEW DoctorView AS SELECT PH, DO,FarmState,AUTOMATED FROM FishFarm where ID='"+ ID +"';";
+            return dbMan.ExecuteTableQuery(query);
+        }
+        public void TerminateConnection()
+        {
+            dbMan.CloseConnection();
+        }
+
+    }
+}
